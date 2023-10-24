6d98e00d3287ce1b3acae315ca21637ea9972934
nishant-jha72 <145684364+nishant-jha72@users.noreply.github.com>
2023-10-24 22:48:34 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // You can add your logic here to handle the form submission, e.g., sending an email or storing data.
+    alert('Form submitted!');
+});
+var red=document.getElementsByClassName('red')
+red.style.color="red"
